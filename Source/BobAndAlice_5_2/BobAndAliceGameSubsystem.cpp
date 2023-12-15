7f18c74ed64f22b4d0dded86fd1b263e9f47cb9d
Vincenzo Pucella <vinnypucella@gmail.com>
2023-12-14 21:40:13 -0500

    Pushing final version
    - Step one : 80% --> Interface set up
    
    - Step two : 100% --> Functioning perfectly
    
    - Step three : 80% --> UI change for collected package on level change not functioning properly
    
    - Step four : 90% --> Functioning properly

---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -0,0 +1,41 @@
+﻿// BobAndAliceGameSubsystem.cpp
+
+#include "BobAndAliceGameSubsystem.h"
+
+void
+UBobAndAliceGameSubsystem::Initialize (FSubsystemCollectionBase &Collection)
+{
+	Super::Initialize (Collection);
+
+	
+}
+
+void
+UBobAndAliceGameSubsystem::Deinitialize ()
+{
+	Super::Deinitialize ();
+}
+
+void
+UBobAndAliceGameSubsystem::SaveTimerToSubSystem(const float TimeLimit)
+{
+	LocalTimer = TimeLimit;
+}
+
+float
+UBobAndAliceGameSubsystem::GetLocalSavedTimer () const
+{
+	return LocalTimer;
+}
+
+void
+UBobAndAliceGameSubsystem::SaveCollectedToSubSystem (const bool isCollected)
+{
+	Collect = isCollected;
+}
+
+bool
+UBobAndAliceGameSubsystem::GetCollected () const
+{
+	return Collect;
+}
---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "BobAndAliceGameSubsystem.generated.h"
+
+UCLASS()
+class BOBANDALICE_5_2_API UBobAndAliceGameSubsystem : public UGameInstanceSubsystem
+{
+  GENERATED_BODY()
+
+
+  public:
+
+  virtual void Initialize (FSubsystemCollectionBase &Collection) override;
+  virtual void Deinitialize () override;
+
+  UFUNCTION (BlueprintCallable)
+  void SaveTimerToSubSystem (const float TimeLImit);
+
+  UFUNCTION (BlueprintCallable,BlueprintPure)
+  float GetLocalSavedTimer () const;
+
+  UFUNCTION (BlueprintCallable)
+  void SaveCollectedToSubSystem (const bool isCollected);
+
+  UFUNCTION (BlueprintCallable, BlueprintPure)
+  bool GetCollected () const;
+
+  private:
+  float LocalTimer = 45;
+	  bool Collect = false;
+};
